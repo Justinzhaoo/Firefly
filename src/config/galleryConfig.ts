0bcae4b672eb977e3bc7e55a4458b2323473f792
Justinzhaoo <justinzhaoo@users.noreply.github.com>
2026-08-10 02:25:18 +0800

    chore: delete album - aa

--- src/config/galleryConfig.ts
+++ src/config/galleryConfig.ts
@@ -5,11 +5,6 @@ export const galleryConfig: GalleryConfig = {
 	// 相册列表
 	albums: [
 		{
-			id: "aa",
-			name: "111",
-			date: "2026-08-09",
-		},
-		{
 			id: "222",
 			name: "开心的事情",
 			description: "好玩的",
